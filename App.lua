7c0b9c283e9d66d12a78b0ac514b41600d372138
jlelz <79492325+jlelz@users.noreply.github.com>
2026-03-06 05:48:13 -0600

    Attempting to solve two issues: delay of chat loading and combat message tainting

--- App.lua
+++ App.lua
@@ -366,9 +366,12 @@ Addon.APP:SetScript( 'OnEvent',function( self,Event,AddonName )
             local BNId = select( 13,... );
             local IconReplacement = select( 17,... );
 
-            -- Fucking... Blizzard
-            if( Addon:IsRetail() and InCombatLockdown() ) then
-                return false;
+            -- During lockdown, don't filter. Instead, send back to default chat system
+            if( ( Addon:IsRetail() and InCombatLockdown() ) or ( C_ChatInfo and C_ChatInfo.InChatMessagingLockdown() ) ) then
+                if( Addon.APP:GetValue( 'Debug' ) ) then
+                    Addon.FRAMES:Debug( 'InCombatLockdown','Sending back/not filtering: ',MessageText );
+                end
+                return false,MessageText, ...
             end
 
             local Prefix,ABBREV,Queued,_,_,_,Tank,Healer,DPS = strsplit( ':',MessageText );
@@ -807,34 +810,20 @@ Addon.APP:SetScript( 'OnEvent',function( self,Event,AddonName )
             Addon.FRAMES:Notify( 'Done' );
         end
 
-        local Iterator = 1;
-        local Timer = 10; if( Addon:IsRetail() ) then Timer = 20; end;
-        -- todo: solve issue where we can't join channels due to IsFlying()
-        -- seems rather silly that the game can't join channels when you log in while flying
-        --
-        -- the problem this causes is that since the chat channels are not available,
-        -- their corresponding channel colors in the db get blown away and then we have to
-        -- reconfigure them once we land
-        -- 
-        -- possibly fix in jChat/Chatframe.lua Init(); presumably, that's where the db values
-        -- are wiped during login
-        --
-        -- edit: debugs have been put in place to check on this...
         Addon.FRAMES:Notify( 'Prepping...please wait' );
-        Addon.APP:RegisterEvent( 'UPDATE_CHAT_WINDOWS' );
+        Addon.APP:RegisterEvent( 'PLAYER_LOGIN' );
         Addon.APP:HookScript( 'OnEvent',function( self,Event,AddonName )
-            if( not( Iterator > 1 ) ) then
-                C_Timer.After( Timer,function()
+            if( Event == 'PLAYER_LOGIN' ) then
+                Addon.DB:Init();
+                --Addon.DB:Reset();
 
-                    Addon.DB:Init();
-                    --Addon.DB:Reset();
+                Addon.CHAT:Init();
+                Addon.CONFIG:Init();
+                Addon.APP:Init();
 
-                    Addon.CHAT:Init();
-                    Addon.CONFIG:Init();
-                    Addon.APP:Init();
+                Addon.FRAMES:Notify( 'Done' );
+                Addon.APP:UnregisterEvent( 'PLAYER_LOGIN' );
 
-                end );
-                Iterator = Iterator+1;
             end
         end );
         self:UnregisterEvent( 'ADDON_LOADED' );
